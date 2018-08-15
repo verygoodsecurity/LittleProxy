c34779fb2f7c67faa6cb62735689b992763f61ae
Slava Fomin <slava.fomin@verygood.systems>
2018-08-16 02:29:14 +0300

    Reverts original code when initial request is released

--- src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
+++ src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
@@ -945,17 +945,16 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
 
         if (shouldForwardInitialRequest) {
             LOG.debug("Writing initial request: {}", initialRequest);
-            doWrite(initialRequest);
+            write(initialRequest);
         } else {
-
-            // we're now done with the initialRequest: it's either been forwarded to the upstream server (HTTP requests), or
-            // completely dropped (HTTPS CONNECTs). if the initialRequest is reference counted (typically because the HttpObjectAggregator is in
-            // the pipeline to generate FullHttpRequests), we need to manually release it to avoid a memory leak.
-            if (initialRequest instanceof ReferenceCounted) {
-                ((ReferenceCounted)initialRequest).release();
-            }
-
             LOG.debug("Dropping initial request: {}", initialRequest);
+        }
+
+        // we're now done with the initialRequest: it's either been forwarded to the upstream server (HTTP requests), or
+        // completely dropped (HTTPS CONNECTs). if the initialRequest is reference counted (typically because the HttpObjectAggregator is in
+        // the pipeline to generate FullHttpRequests), we need to manually release it to avoid a memory leak.
+        if (initialRequest instanceof ReferenceCounted) {
+            ((ReferenceCounted)initialRequest).release();
         }
     }
 
