647f911685c405c66038d590523647fa22dac8ab
Mahmoud Abdelkader <mabdelkader@gmail.com>
2020-03-01 19:48:15 -0800

    Start a local Jetty webserver to do integration tests for Brotli

--- src/test/java/org/littleshoot/proxy/BrotliEncoderDecoderTest.java
+++ src/test/java/org/littleshoot/proxy/BrotliEncoderDecoderTest.java
@@ -2,6 +2,9 @@ package org.littleshoot.proxy;
 
 import com.google.common.io.Resources;
 
+import org.apache.http.HttpHost;
+import org.eclipse.jetty.server.Server;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,19 +14,46 @@ import io.netty.handler.codec.compression.BrotliDecoder;
 import static org.junit.Assert.assertArrayEquals;
 
 public class BrotliEncoderDecoderTest extends AbstractProxyTest {
-    @Override
-    protected void setUp() {
-        this.proxyServer = bootstrapProxy()
-                .withPort(0)
-                .start();
-    }
-  private static byte[] loadUncompressedSample() throws IOException {
-     return Resources.toByteArray(BrotliEncoderDecoderTest.class.getResource("/brotli/a100.txt"));
-   }
 
-   private static byte[] loadBrotliCompressedSample() throws IOException {
-     return Resources.toByteArray(BrotliEncoderDecoderTest.class.getResource("/brotli/a100.txt.br"));
-   }
+  private Server webServer;
+
+  @Before
+  @Override
+  public void runSetUp() throws Exception {
+    System.out.println("Got here!");
+    webServer = TestUtils.startWebServer(true);
+
+    // find out what ports the HTTP and HTTPS connectors were bound to
+    httpsWebServerPort = TestUtils.findLocalHttpsPort(webServer);
+    if (httpsWebServerPort < 0) {
+      throw new RuntimeException("HTTPS connector should already be open and listening, but port was " + webServerPort);
+    }
+
+    webServerPort = TestUtils.findLocalHttpPort(webServer);
+    if (webServerPort < 0) {
+      throw new RuntimeException("HTTP connector should already be open and listening, but port was " + webServerPort);
+    }
+
+    webHost = new HttpHost("127.0.0.1", webServerPort);
+    httpsWebHost = new HttpHost("127.0.0.1", httpsWebServerPort, "https");
+
+    setUp();
+  }
+
+  @Override
+  protected void setUp() {
+    this.proxyServer = bootstrapProxy()
+        .withPort(0)
+        .start();
+  }
+
+  private static byte[] loadUncompressedSample() throws IOException {
+    return Resources.toByteArray(BrotliEncoderDecoderTest.class.getResource("/brotli/a100.txt"));
+  }
+
+  private static byte[] loadBrotliCompressedSample() throws IOException {
+    return Resources.toByteArray(BrotliEncoderDecoderTest.class.getResource("/brotli/a100.txt.br"));
+  }
 
   @Test
   public void decompressBrotliContents() throws IOException {
