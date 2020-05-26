6cd25556d61983db5ae095ac60224d98e374bde1
ozasymenko <oleg.zasymenko@vgs.io>
2020-05-26 19:53:35 +0300

    reverted ProxyToServerConnection changes 4

--- src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
+++ src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
@@ -78,8 +78,6 @@ import static org.littleshoot.proxy.impl.ConnectionState.HANDSHAKING;
  * </p>
  */
 
-
-
 @Sharable
 public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
     private final ClientToProxyConnection clientConnection;
